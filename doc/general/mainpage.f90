a03df364735d5bb061d31ac7786943cd5b5c618e
eero <eero@146eac83-2b0f-0410-9363-9ebcc3d868b9>
2006-10-06 07:13:34 +0000

    web page update
    git-svn-id: svn://dougdevel.org/doug/trunk@132 146eac83-2b0f-0410-9363-9ebcc3d868b9

--- doc/general/mainpage.f90
+++ doc/general/mainpage.f90
@@ -1,7 +1,7 @@
 !> \mainpage DOUG - Domain Decomposition on Unstructured Grids. 
 !>
 !> See also wiki pages for DOUG at: 
-!> <A HREF="http://kheiron.at.mt.ut.ee/wiki">http://kheiron.at.mt.ut.ee/wiki</A>.
+!> <A HREF="http://www.dougdevel.org/wiki">http://www.dougdevel.org/wiki</A>.
 !!
 !! \section doc Documentation
 !!
