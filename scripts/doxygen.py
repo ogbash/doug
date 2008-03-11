2ed26d31a517164b633a4b01e59d9cbe8b6b8442
oleg <oleg@146eac83-2b0f-0410-9363-9ebcc3d868b9>
2008-03-11 17:06:04 +0000

    Add simple documentaion genertion script.
    git-svn-id: svn://dougdevel.org/doug/trunk@242 146eac83-2b0f-0410-9363-9ebcc3d868b9

---
+++ scripts/doxygen.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+#: Simple script that generates DOUG documentation and copies it to the
+#: corresponding www directory.
+
+import sys
+import os
+
+log = open("/srv/doug/output/doxygen.out", "w+")
+log.write("ARGS: %s\n" % sys.argv)
+
+rev = sys.argv[2]
+
+orig_dir = os.getcwd()
+os.chdir('/srv/doug/doug-doc')
+try:
+    ret = os.spawnlp(os.P_WAIT, 'svn', 'svn', 'up', '-r', rev)
+    log.write("svn up returned %d\n" % ret)
+
+    os.chdir('doc')
+    ret = os.spawnlp(os.P_WAIT, 'doxygen', 'doxygen')
+    log.write("doxygen returned %d\n" % ret)
+
+    ret = os.system('cp html/* /srv/www/dougdevel.org/doug/docs')
+    log.write("copy returned %d\n" % ret)
+finally:
+    os.chdir(orig_dir)
+
