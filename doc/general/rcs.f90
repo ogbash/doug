54bfa4123d74f99dba5e4a5d46ef8c4e45496a8c
oleg <oleg@146eac83-2b0f-0410-9363-9ebcc3d868b9>
2008-03-13 08:21:46 +0000

    Fixes to the doc from Jan remarks.
    git-svn-id: svn://dougdevel.org/doug/trunk@246 146eac83-2b0f-0410-9363-9ebcc3d868b9

--- doc/general/rcs.f90
+++ doc/general/rcs.f90
@@ -1,11 +1,11 @@
 !> \page p_rcs Robust Coarse Spaces
 !!
-!! \author Oleg Batrashev, (algorithm by Ivan G. Graham, Robert Scheichl and Jan Van lent)
-!!
-!! Implementation is based on the work by Ivan G. Graham, Robert Scheichl and Jan Van lent, "Robust Coarse Spaces for Overlapping Schwarz Methods".
+!! Implementation is based on the work by Ivan Graham, Robert Scheichl and Jan Van lent, "Robust Coarse Spaces for Overlapping Schwarz Methods".
 !!
 !! \section input Input
-!!  - \f$ R_i \f$ - projection matrices to the coarse node supports in the fine space. This also identifies aggregates 
+!!  - \f$ R_i \f$ - injection matrices mapping from the vectors taking 
+!!       values at all points of the fine mesh to vectors taking values at the 
+!!       points in the interior of the support of basis function i. This also identifies aggregates 
 !!       with the overlap included (although no aggregate neighbour information is directly available).
 !!  - \e A - system matrix
 !!  - \e <unavailable, ie may leave out for the first implementation> - neighbour information, i.e. which aggregates overlap.
@@ -25,8 +25,8 @@
 !!
 !! \subsection general General description
 !!
-!! To get restriction matrix we need to solve the problem (related to energy minimization problem?)
-!!    \f[ B g = 1 \mbox{ where } B = \sum\limits_i^{n_c} =  R_i^T A_i^{-1} R_i 
+!! To get restriction matrix we need to solve the problem (\e g - vector of Lagrange multipliers in the energy minimization problem)
+!!    \f[ B g = 1 \mbox{ where } B = \sum\limits_i^{n_c}  R_i^T A_i^{-1} R_i 
 !!       \mbox{ and } A_i = R_i A R_i^T \f]
 !! The CG solver (see preconditioner below) can be found in pcg_forRCS(). Because matrix \e B is not sparse special
 !!   datatype RobustCoarseMtx_mod::SumOfInversedSubMtx has been introduced.
@@ -43,7 +43,8 @@
 !! Preconditioner \e C for the \e B is
 !! \f[ C = \sum\limits_{j=1}^{n_c} R_j^T B_j^{-1} R_j \f]
 !!
-!! \e B's can be calculated by
+!! Multiplying a vector by the matrix C can be implemented efficiently 
+!!   using the following formulas which involve only sparse matrices
 !! \f[
 !!   B_j^{-1} = A_j - A_j
 !!     \left[ \begin{array}{ccc} I_{jk} & \cdots & I_{jl} \end{array} \right]
@@ -62,10 +63,4 @@
 !! The implementation of preconditioner is in 2 subroutines
 !! - precondition_forRCS::initialize() - initializes datatype: creates \e I and \e H matrices
 !! - pcgRobust_mod::precondition_forRCS() applies \e C
-!!
-!! \section unsorted Unsorted Formulas
-!!
-!! The two-level Schwarz preconditioner
-!! \f[ \tilde{B} = \hat{R}^T \hat{A}^{-1} \hat{R} 
-!!   + \sum\limits_{i=1}^{n}{ \breve{R}^T_i \breve{A}_i^{-1} \breve{R}_i}\f]
 !!
