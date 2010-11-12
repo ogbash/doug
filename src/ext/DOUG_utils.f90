85ba2613f7b94d00ccc68be406972714b504a666
Oleg Batrashev <ogbash@gmail.com>
2010-11-12 19:46:25 +0200

    External interface: simple CG now works when called from C (tested with gfortran).

--- src/ext/DOUG_utils.f90
+++ src/ext/DOUG_utils.f90
@@ -1,7 +1,78 @@
 
+!#include <doug_config.h>
+
 !> Initialize DOUG, init_type: 1 - parallel, 2 - serial.
 subroutine ext_DOUG_Init(init_type)
   use DOUG_utils
   integer, intent(in), optional :: init_type  
   call DOUG_Init(init_type)
 end subroutine ext_DOUG_Init
+
+subroutine ext_DOUG_Finalize()
+  use DOUG_utils
+  call DOUG_Finalize()
+end subroutine ext_DOUG_Finalize
+
+! Allocate memory for SpMtx class.
+subroutine alloc_SpMtx(A)
+  use SpMtx_class
+  type(SpMtx),pointer :: A
+  allocate(A)
+  A = SpMtx_New()
+end subroutine alloc_SpMtx
+
+! Allocate memory for Mesh class.
+subroutine alloc_Mesh(M)
+  use Mesh_class
+  type(Mesh),pointer :: M
+  allocate(M)
+  M = Mesh_New()
+end subroutine alloc_Mesh
+
+subroutine ext_parallelDistributeInput(M, A, b, nparts, A_ghost, nlf)
+  use main_drivers
+  use globals, only: sctls
+  implicit none 
+  type(Mesh),     intent(in out) :: M
+  type(SpMtx),    intent(out) :: A
+  real(kind=rk), intent(out) :: b(*)
+  integer, intent(in) :: nparts
+  type(SpMtx),intent(in out) :: A_ghost
+  integer, intent(out) :: nlf
+  
+  real(kind=rk), dimension(:), pointer :: b_
+  integer, dimension(6) :: part_opts
+
+  call parallelDistributeInput(sctls%input_type, M, A, b_, nparts, part_opts, A_ghost)
+  b(:size(b_)) = b_
+  nlf = size(b_)
+  deallocate(b_)
+
+end subroutine ext_parallelDistributeInput
+
+subroutine ext_cg(A, b, M, xl, nlf)
+  use cg_mod
+  implicit none
+
+  type(SpMtx),intent(in out) :: A ! System matrix (sparse)
+  real(kind=rk),dimension(nlf) :: b ! RHS
+  real(kind=rk),dimension(nlf) :: xl ! Solution
+  ! Mesh - aux data for Ax operation
+  type(Mesh),intent(in) :: M
+  integer, intent(in) :: nlf
+  
+  ! Solution statistics
+  type(ConvInf) :: solinf
+
+  real(kind=rk),pointer :: b_(:), xl_(:) ! RHS
+  allocate(b_(size(b)))
+  b_ = b
+  allocate(xl_(size(xl)))
+  xl_ = xl
+
+  call cg(A, b_, xl_, M, solinf=solinf)
+
+  xl = xl_
+  deallocate(b_)
+  deallocate(xl_)
+end subroutine ext_cg
--- src/ext/main_aggr.c
+++ src/ext/main_aggr.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 
-extern ext_doug_init_(int *init_type);
+extern void ext_doug_init_(int *init_type);
+extern void ext_doug_finalize_();
+extern void alloc_spmtx_(void **A);
+extern void alloc_mesh_(void **M);
+extern void ext_paralleldistributeinput_(void *M, void *A, double *b, int *nparts, void *A_ghost, int *nlf);
+extern void ext_cg_(void *A, double *b, void *M, double *xl, int *nlf);
 
-int main()
+int main(int argc, char **argv)
 {
+  int i;
+  void *A, *A_ghost;
+  void *M;
+  int nparts=1;
   int init_type=1;
+  double xl[49];
+  double b[49];
+  int nlf;
+
   ext_doug_init_(&init_type); // parallel
-  printf("Initialized\n");
+  alloc_spmtx_(&A);
+  alloc_spmtx_(&A_ghost);
+  alloc_mesh_(&M);
+  printf("Initialized, matrix %x, mesh %x\n", A, M);
+  ext_paralleldistributeinput_(M, A, b, &nparts, A_ghost, &nlf);
+
+  ext_cg_(A, b, M, xl, &nlf);
+
+  ext_doug_finalize_();
+
+  printf("result size=%d:\n", nlf);
+  for(i=0; i<nlf; i++) {
+    printf("%f ", xl[i]);
+  }
+
   return 0;
 }
