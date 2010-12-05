9c7b6610ea45c3447843c1e246fad3cb037bdbc4
Oleg Batrashev <ogbash@gmail.com>
2010-12-05 12:12:54 +0200

    Move mysql request to separate class.

--- scripts/doug/testmysql.py
+++ scripts/doug/testmysql.py
@@ -38,68 +38,130 @@ class TestStatus:
     ERROR=4
     STRS = ['NONE','RUNNING','SUCCESS','FAILURE','ERROR']
 
+class MysqlTestStore:
+    "Methods to store test to Mysql database."
+
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = MySQLdb.connect(host, user, password, database)
+        self.cursor = self.connection.cursor()
+
+    def insertTestRun(self, conf):
+        servername = conf.get("dougtest", "info-server")
+        revision = conf.get("dougtest","info-svn")
+        revision = revision or None
+        gitversion = conf.get("dougtest","info-git")
+        gitversion = gitversion or None
+        compiler = conf.get("dougtest","info-fc")
+        mpi = conf.get("dougtest","info-mpi")
+            
+        self.cursor.execute("insert into testruns (servername, svnrevision, gitversion, fcompiler, mpi, starttime)"
+                            " values (%s, %s, %s, %s, %s, %s)",
+                            (servername, revision, gitversion, compiler, mpi, datetime.today()))
+        self.ID = self.cursor.lastrowid
+
+    def updateTestRunStop(self, time):
+        self.cursor.execute("update testruns set endtime=%s"
+                            " where id=%s", (time, self.ID))
+
+    def updateTestRunError(self, errdesc):
+        self.cursor.execute("update testruns set errortext=%s where id=%s",
+                            (errdesc, self.ID))
+
+
+    def insertTest(self, testname, time, conf):
+        "time - test start time"
+        self.cursor.execute("insert into testresults (name, testrun_ID, starttime, "
+                            " executable, inputtype, method, solver, nproc, levels, overlap, smoothers)"
+                            " values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                            (testname, self.ID, time,
+                             conf.get("doug", "executable"),
+                             conf.get("doug-controls", "input_type"),
+                             conf.get("doug-controls", "method"),
+                             conf.get("doug-controls", "solver"),
+                             conf.get("doug", "nproc"),
+                             conf.get("doug-controls", "levels"),
+                             conf.get("doug-controls", "overlap"),
+                             conf.get("doug-controls", "smoothers")))
+        ID = self.cursor.lastrowid
+        self.cursor.execute("update testresults set status=%s where id=%s",
+                            (TestStatus.RUNNING, ID))
+        return ID
+
+    def updateTestStop(self, ID, time):
+        self.cursor.execute("update testresults set endtime=%s where id=%s",
+                            (time, ID))
+
+    def updateTestStatus(self, ID, status):
+        self.cursor.execute("update testresults set status=%s where id=%s",
+                            (status, ID))
+
+    def updateTestError(self, ID, errdesc):
+        self.cursor.execute("update testresults set errortext=%s where id=%s",
+                            (errdesc, ID))
+
+    def updateTestProfileInfo(self, ID, conf):
+        c = conf
+        sqlr = ("update testresults set iterations=%d, iterations_time=%f, preconditioner_time=%f," + 
+                " fine_aggrs=%d, coarse_aggrs=%d where id=%s") % (
+            c.getint('doug-profile', 'pcg-iterations'),
+            c.getfloat('doug-profile', 'iterations-time'),
+            c.getfloat('doug-profile', 'preconditioner-time'),
+            c.has_option('doug-profile', 'fine-aggregates') and c.getint('doug-profile', 'fine-aggregates') or -1,
+            c.has_option('doug-profile', 'coarse-aggregates') and c.getint('doug-profile', 'coarse-aggregates') or -1,
+            ID)
+        self.cursor.execute(sqlr)
+
+    def insertTestFileContent(self, ID, filename, content):
+        self.cursor.execute("insert into testresultfiles (testresult_ID, name)"
+                            " values (%s, %s)", (ID, filename))
+        fileID = self.cursor.lastrowid
+        self.cursor.execute("update testresultfiles set content=%s"
+                            " where id=%s", (MySQLdb.Binary(content), fileID))
+
+                        
 class DougMySQLTestResult(unittest.TestResult):
 	"""Saves DOUG test results into MySQL DB."""
 	
 	def __init__(self, host, user, password, database, conf):
 		unittest.TestResult.__init__(self)
-                self.host = host
-                self.user = user
-                self.database = database
+                self.store = MysqlTestStore(host,user,password,database)
                 self.conf = conf
-                self.connection = MySQLdb.connect(host, user, password, database)
-                self.cursor = self.connection.cursor()
-                self._createTestRun()
+                self.store.insertTestRun(self.conf)
 
 	def startTest(self, test):
             test.acquire()
             unittest.TestResult.startTest(self, test)
             conf = test.dougExecution.config
-            self.cursor.execute("insert into testresults (name, testrun_ID, starttime, "
-                                " executable, inputtype, method, solver, nproc, levels, overlap, smoothers)"
-                                " values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                                (test.testname, self.ID, datetime.today(),
-                                 conf.get("doug", "executable"),
-                                 conf.get("doug-controls", "input_type"),
-                                 conf.get("doug-controls", "method"),
-                                 conf.get("doug-controls", "solver"),
-                                 conf.get("doug", "nproc"),
-                                 conf.get("doug-controls", "levels"),
-                                 conf.get("doug-controls", "overlap"),
-                                 conf.get("doug-controls", "smoothers")))
-            test.ID = self.cursor.lastrowid
-            self.cursor.execute("update testresults set status=%s where id=%s",
-                                (TestStatus.RUNNING, test.ID))
+            test.ID = self.store.insertTest(test.testname, datetime.today(), conf)
 
 	def stopTest(self, test):
             test.free()            
             unittest.TestResult.stopTest(self, test)
-            self.cursor.execute("update testresults set endtime=%s where id=%s",
-                                (datetime.today(), test.ID))
+            self.store.updateTestStop(test.ID, datetime.today())
 
 	def addError(self, test, err):
             if test!=None:
                 # test error
                 unittest.TestResult.addError(self, test, err)
-                self.cursor.execute("update testresults set status=%s where id=%s",
-                                    (TestStatus.ERROR, test.ID))
+                self.store.updateTestStatus(test.ID, TestStatus.ERROR)
                 self._storeError(test, err)
             else:
                 # generic error. hack :(, must be removed?
 		errtp, errval, errtb = err
-                self.cursor.execute("update testruns set errortext=%s where id=%s",
-                                    (str(errval), self.ID))
+                self.store.updateTestRunError(str(errval))
 
 	def addFailure(self, test, err):
 		unittest.TestResult.addFailure(self, test, err)
-                self.cursor.execute("update testresults set status=%s where id=%s",
-                                    (TestStatus.FAILURE, test.ID))
+                self.store.updateTestStatus(test.ID, TestStatus.FAILURE)
                 self._storeError(test, err)
 
 	def addSuccess(self, test):
 		unittest.TestResult.addSuccess(self, test)
-                self.cursor.execute("update testresults set status=%s where id=%s",
-                                    (TestStatus.SUCCESS, test.ID))
+                self.store.updateTestStatus(test.ID, TestStatus.SUCCESS)
 
 		if hasattr(test, "files"):
 			for fname, descr in test.files:
@@ -107,16 +169,8 @@ class DougMySQLTestResult(unittest.TestResult):
 
                 # update profiling values
                 if test.resultConfig.has_section('doug-profile'):
-                    c = test.resultConfig
-                    sqlr = ("update testresults set iterations=%d, iterations_time=%f, preconditioner_time=%f," + \
-                            " fine_aggrs=%d, coarse_aggrs=%d where id=%s") % (
-                        c.getint('doug-profile', 'pcg-iterations'),
-                        c.getfloat('doug-profile', 'iterations-time'),
-                        c.getfloat('doug-profile', 'preconditioner-time'),
-                        c.has_option('doug-profile', 'fine-aggregates') and c.getint('doug-profile', 'fine-aggregates') or -1,
-                        c.has_option('doug-profile', 'coarse-aggregates') and c.getint('doug-profile', 'coarse-aggregates') or -1,
-                        test.ID)
-                    self.cursor.execute(sqlr)
+                    conf = test.resultConfig
+                    self.store.updateTestProfileInfo(test.ID, conf)
 
 	def stop(self):
 		unittest.TestResult.stop(self)
@@ -132,43 +186,23 @@ class DougMySQLTestResult(unittest.TestResult):
 
                 self.connection.close()
 
-        def _createTestRun(self):
-            servername = self.conf.get("dougtest", "info-server")
-            revision = self.conf.get("dougtest","info-svn")
-            revision = revision or None
-            gitversion = self.conf.get("dougtest","info-git")
-            gitversion = gitversion or None
-            compiler = self.conf.get("dougtest","info-fc")
-            mpi = self.conf.get("dougtest","info-mpi")
-            
-            self.cursor.execute("insert into testruns (servername, svnrevision, gitversion, fcompiler, mpi, starttime)"
-                                " values (%s, %s, %s, %s, %s, %s)",
-                                (servername, revision, gitversion, compiler, mpi, datetime.today()))
-            self.ID = self.cursor.lastrowid
-
         def _finishTestRun(self):
-            self.cursor.execute("update testruns set endtime=%s"
-                                " where id=%s", (datetime.today(), self.ID))
+            self.store.updateTestRunStop(datetime.today())
 
         def _storeError(self, test, err):
-		errtp, errval, errtb = err
-                self.cursor.execute("update testresults set errortext=%s where id=%s",
-                                    (str(errval), test.ID))
+            errtp, errval, errtb = err
+            self.store.updateTestError(test.ID, str(errval))
                 
-		if hasattr(test, "files"):
-			for fname, descr in test.files:
-				self._addFile(fname, test)
+            if hasattr(test, "files"):
+                for fname, descr in test.files:
+                    self._addFile(fname, test)
 
         def _addFile(self, name, test):
-            self.cursor.execute("insert into testresultfiles (testresult_ID, name)"
-                                " values (%s, %s)", (test.ID, os.path.basename(name)))
-            fileID = self.cursor.lastrowid
-
             fd = open(name, "r")
             try:
                 fcontent = fd.read()
             except IOError, e:
                 LOG.error("Could not open output file %s: %s" % (name, e))
             else:
-                self.cursor.execute("update testresultfiles set content=%s"
-                                    " where id=%s", (MySQLdb.Binary(fcontent), fileID))
+                self.store.insertTestFileContent(test.ID, os.path.basename(name), fcontent)
+            fd.close()
